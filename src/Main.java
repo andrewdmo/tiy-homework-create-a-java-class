e092034415f7bb9fa743f5553c934a7d09846a5d
andrewdmo <andrewdmo@gmail.com>
2016-08-08 09:50:55 -0400

    first

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by andrewdmo on 8/4/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Baseballpositions player = new Baseballpositions("Babe", "Pitcher", 300000, true, false);
+
+        //player.salaryplussigningbonus();
+
+        //salary.salaryplussigningbonus()
+
+        System.out.println(player);
+    }
+}
